797b207f1d1c06b3bc25733745aeb9fa8d9742b7
lisongyu <269876940@qq.com>
2015-02-07 17:04:39 +0800

    提交

--- requirejs-demo/js/web/masonry1.js
+++ requirejs-demo/js/web/masonry1.js
@@ -1,4 +1,4 @@
-define(['jquery', './masonry/masonry.pkgd.min', './imagesloaded/imagesloaded'], function($, Masonry, imagesloaded) {
+define(['./masonry/masonry.pkgd.min','./imagesloaded/imagesloaded','jquery'], function(Masonry,imagesloaded,$) {
     var container = document.getElementById("container");
     var more = document.getElementById("more");
     var msnry = new Masonry(container);
@@ -6,13 +6,46 @@ define(['jquery', './masonry/masonry.pkgd.min', './imagesloaded/imagesloaded'], 
     imagesloaded(container, function() {
         msnry.layout();
     });
+    var len=$('.item').length;
     $(more).on("click", addItem);
 
     function addItem() {
-        var html = $('<div class="item">新添加</div><div class="item">新添加</div>');
-        //添加到dom
-        $(container).append(html);
-        //通知masonry已添加的dom元素
-        msnry.appended(html);
+
+        console.log(len);
+        var request=$.ajax({
+            url: "json/data.json",
+            type: "get",
+            data:'5',
+            dataType: "json",
+            cache: false
+
+        });
+        request.done(function(data) {
+            var arrPhone = [];
+            //        console.log(arr.toString()==data[0].point);
+            $(data).each(function(index, value) {
+                if(index>len){
+                    if(index<(len+4)){
+                        var oIndex = data[index];
+//                    console.log(index);
+                        var html = $('<div class="item"><img src='+oIndex.imgSrc+' alt=""/></div>');
+                        //添加到dom
+                        $(container).append(html);
+                        //通知masonry已添加的dom元素
+                        msnry.appended(html);
+
+                    }
+
+                }
+
+
+
+
+            });
+            len+=3;
+
+        });
+
+
     }
 })
